9ef0d8280d94b4e62ec21759fd1154b25c352bfa
performanceArtist1 <nakedant19@gmail.com>
2019-07-20 21:42:36 +0600

    Fix value bug #4

--- src/js/model/model.ts
+++ src/js/model/model.ts
@@ -63,7 +63,7 @@ class Model extends Observable {
 
   validate(key: string, value: number | string | boolean) {
     const newValue = checkType(key, value);
-    const { state } = this._model;
+    const state = this.getState();
 
     if (newValue instanceof SliderError) {
       return newValue;
@@ -73,12 +73,13 @@ class Model extends Observable {
       case 'value':
       case 'firstValue':
       case 'secondValue':
+        if (newValue >= state.max) return state.max;
+        if (newValue <= state.min) return state.min;
+
         if (key === 'firstValue' && newValue >= state.secondValue)
           return state.firstValue;
         if (key === 'secondValue' && newValue <= state.firstValue)
           return state.secondValue;
-        if (newValue > state.max) return state.max;
-        if (newValue < state.min) return state.min;
         return (
           state.min +
           state.step *
@@ -95,13 +96,6 @@ class Model extends Observable {
       case 'step':
         if (newValue <= 0 || newValue > state.max - state.min)
           return new SliderError(ErrorType.STEP, key);
-
-        const newStep = newValue as number;
-        const remainder = (state.max - state.min) % newStep;
-        const quotient = Math.floor((state.max - state.min) / newStep);
-
-        if (remainder !== 0) this.setState({ max: (quotient + 1) * newStep });
-
         return newValue;
       default:
         return newValue;
@@ -116,12 +110,12 @@ class Model extends Observable {
 
     const { state, meta } = this._model;
     const setValue = (key: string, newValue: number | string | boolean) => {
-      const res = this.validate(key, newValue);
-      if (res instanceof SliderError) {
-        meta.errors.push(res.getMessage());
-        res.show();
+      const result = this.validate(key, newValue);
+      if (result instanceof SliderError) {
+        meta.errors.push(result.getMessage());
+        result.show();
       } else {
-        state[key] = res;
+        state[key] = result;
       }
     };
     const keys = Object.keys(options);
@@ -134,7 +128,8 @@ class Model extends Observable {
     });
 
     // set value after options update to ensure it's valid
-    const interval = state.interval || options.interval;
+    const interval =
+      options.interval === undefined ? state.interval : options.interval;
 
     if (interval) {
       const first =
